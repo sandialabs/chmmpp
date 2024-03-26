4c50a03d27e4b600c754d78cf25f9bc7667c1aa8
whart222 <whart222@gmail.com>
2024-03-26 12:05:11 -0600

    Removing 'mip' directory

--- chmmpp/mip/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "test.hpp"
-
-#include <iostream>
-
-namespace chmmpp {
-
-void run_test() { std::cout << "CHMMPP TEST" << std::endl; }
-
-}  // namespace chmmpp
--- chmmpp/mip/test.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace chmmpp {
-
-void run_test();
-
-}  // namespace chmmpp
